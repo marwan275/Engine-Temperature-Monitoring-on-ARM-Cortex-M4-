2ce9e99a7a173baf78c240f30b839c1fb1a42760
Marwan Mobark <88746411+marwan275@users.noreply.github.com>
2024-04-09 02:46:03 +0200

    Deleted files

--- adc_cfg.c
+++
@@ -1,4 +0,0 @@
-#include <adc_cfg.h>
-
-ADC_config ADC_Configuartions[NUMBER_ADC_MODULES_CONFIGURED] = { {
-        .module_num = ADC0, .mode = ADC_POOLING, .pin_num = PIN_3 }, };
--- adc_cfg.h
+++
@@ -1,9 +0,0 @@
-#ifndef ADC_CFG_H_
-#define ADC_CFG_H_
-
-#include "ADC.h"
-
-#define NUMBER_ADC_MODULES_CONFIGURED 1
-#define ADC0_INDEX 0
-
-#endif
